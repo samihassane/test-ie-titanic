7c5b66ff41e24042023944d14d7206bb164122df
samihassane <sami.hassane@student.ie.edu>
2021-12-15 10:44:13 +0000

    Add printed message

--- str_utils.py
+++ str_utils.py
@@ -1,2 +1,10 @@
 def extract_titles(name):
     return name.split()[0]
+
+
+print(__name__)
+
+if __name__ == "__main__":
+    print("Script ran successfull")
+else:
+    print("Thank you for importing str_utils")
