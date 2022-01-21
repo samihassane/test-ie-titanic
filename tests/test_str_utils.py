ef4a1687f71fc34e5470ee05f0145154fd78b457
samihassane <sami.hassane@student.ie.edu>
2022-01-21 10:53:20 +0000

    Parametrize tests

--- tests/test_str_utils.py
+++ tests/test_str_utils.py
@@ -1,4 +1,5 @@
-#Filename has to start with test_
+# Filename has to start with test_
+
 
 def test_dummy():
     # you can assert for whatever you want,
@@ -6,16 +7,18 @@ def test_dummy():
     # assert [1,2,3] == [1,2,3,4] to check for a mistake
     assert True
 
+
 from titanic_utils.str_utils import extract_titles
+import pytest
 
-
-def test_extract_titles_return_expected_output():
-    #1. Preconditions
-    expected_input = "Mr. John Doe"
-    expected_output = "Mr."
-
-    #2 Run the code
+# @ = "Decorators"
+@pytest.mark.parametrize(
+    "expected_input, expected_output",
+    [("Mr. John Doe", "Mr."), ("Miss. Jane Doe", "Miss."), ("Dr. Manhattan", "Dr.")],
+)
+def test_extract_titles_return_expected_output(expected_input, expected_output):
+    # 2 Run the code
     output = extract_titles(expected_input)
 
-    #3 Verify
+    # 3 Verify
     assert output == expected_output
