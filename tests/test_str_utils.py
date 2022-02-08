3e8cc37b9bbbbd3d8d9893b20472b6412a0e2ed6
samihassane <sami.hassane@student.ie.edu>
2022-02-08 10:53:37 +0000

    test for gibberish

--- tests/test_str_utils.py
+++ tests/test_str_utils.py
@@ -1,15 +1,15 @@
 # Filename has to start with test_
+import pytest
+from titanic_utils.str_utils import extract_titles
 
+import hypothesis.strategies as st
+from hypothesis import given
 
 def test_dummy():
     # you can assert for whatever you want,
     # *but* peoply usally avoid them in production code
     # assert [1,2,3] == [1,2,3,4] to check for a mistake
     assert True
-
-
-from titanic_utils.str_utils import extract_titles
-import pytest
 
 # @ = "Decorators"
 @pytest.mark.parametrize(
@@ -27,3 +27,14 @@ def test_extract_titles_return_expected_output(expected_input, expected_output):
 def test_extract_titles_without_dot_raises_error():
     with pytest.raises(ValueError):
         extract_titles("Alabama")
+
+@given(
+    st.text(
+        alphabet=st.characters(
+             blacklist_characters=["."]
+        )
+    )
+)
+def test_extract_titles_with_gibberish_without_dots_raises_error(input_text):
+    with pytest.raises(ValueError):
+        extract_titles(input_text)
